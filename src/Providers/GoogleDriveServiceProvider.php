5fa95ac63a19da8bce2abaa07996abdd2c72125e
Michael Umschlag <umschlag@gmx.net>
2020-06-03 17:01:29 +0000

    Pass the configuration either directly as the JSON file, or as base64 encoded JSON string

--- src/Providers/GoogleDriveServiceProvider.php
+++ src/Providers/GoogleDriveServiceProvider.php
@@ -14,32 +14,32 @@ use League\Flysystem\Config;
  * See e.g. https://laravel.com/docs/7.x/packages#package-discovery
  *
  * @author umschlag
- *        
+ *
  */
 class GoogleDriveServiceProvider extends ServiceProvider
 {
-
+    
     /**
      * The Google Client instance
      *
      * @var \Google_Client|null
      */
     public $googleClient = null;
-
+    
     /**
      * The Google Drive Service instance.
      *
      * @var \Google_Service_Drive|null
      */
     public $googleServiceDrive = null;
-
+    
     /**
      * The configuration object for our GDrive Adpater
      *
      * @var Config|null
      */
     public $config = null;
-
+    
     /**
      * The defaut / fallback configuration of our GDrive Adapter.
      * They are passed to the Config instance.
@@ -59,9 +59,9 @@ class GoogleDriveServiceProvider extends ServiceProvider
         // @todo implement / test that. Most likely the cache will get confused
         'skipTrash' => false,
         // The json file with the serivce account configuration
-        'serviceConf' => 'gdrive_service_conf.json',
+        'serviceConf' => 'gdrive_service_conf.json'
     ];
-
+    
     /**
      * Bootstrap the application services.
      *
@@ -73,42 +73,42 @@ class GoogleDriveServiceProvider extends ServiceProvider
         /*
          * Add a "google" driver for the FilesystemAdapter
          */
-        Storage::extend('google',
+        Storage::extend('gdrive',
             function ($app, $config) {
                 
                 // Get a configuration object
                 $config = $this->getConfig($config, $this->configFallback);
                 
-
                 // Get the Google_Client instance
                 $client = $this->getGoogleClient();
-
-                // Configure the client
-                $client->setApplicationName('YPulse-Storage');
-                $client->setAuthConfig( $config->get('serviceConf') );
+                $client->setApplicationName('Flysystem-GDrive');
+                
+                // Either take the configuration directly from the JSON file...
+                if ($config->get('serviceConfFile', '') != '') {
+                    $client->setAuthConfig($config->get('serviceConfFile'));
+                } // ... or the base64 encode json string...
+                elseif ($config->get('serviceConf', '') != '') {
+                    $client->setAuthConfig(json_decode(base64_decode($config->get('serviceConf')), true));
+                } // ... or we cannot help here.
+                else {
+                    throw new \InvalidArgumentException(
+                        'GDrive configration not set. Either set serviceConfFile or serviceConf');
+                }
+                
+                // Let's ask for all GDrive scopes
                 $client->addScope('https://www.googleapis.com/auth/drive');
-
-                // if(env('GOOGLE_DRIVE_API_KEY') != '') {
-                // $client->setDeveloperKey(env('GOOGLE_DRIVE_API_KEY'));
-                // } else {
-                // return logs()->error('GOOGLE_DRIVE_API_KEY not set');
-                // $client->setClientId($config['clientId']);
-                // $client->setClientSecret($config['clientSecret']);
-                // $client->refreshToken($config['refreshToken']);
-                // }
-
+                
                 // Pass the Client to the Service
                 $service = $this->getGoogleServiceDrive($client);
-
-
+                
                 // Initiate our GDrive Adapter
                 $adapter = new GoogleDriveAdapter($service, $config);
-
+                
                 // Finally get the Flysystem with our Adapter
                 return new \League\Flysystem\Filesystem($adapter);
             });
     }
-
+    
     /**
      * Get the Google_Client instance.
      *
@@ -118,9 +118,9 @@ class GoogleDriveServiceProvider extends ServiceProvider
     {
         if ($this->googleClient == null)
             $this->googleClient = new \Google_Client();
-        return $this->googleClient;
+            return $this->googleClient;
     }
-
+    
     /**
      * Set the Google_Client instance.
      *
@@ -132,7 +132,7 @@ class GoogleDriveServiceProvider extends ServiceProvider
         $this->googleClient = $googleClient;
         return $this;
     }
-
+    
     /**
      * Get the Google Drive Service instance.
      *
@@ -143,9 +143,9 @@ class GoogleDriveServiceProvider extends ServiceProvider
     {
         if ($this->googleServiceDrive == null)
             $this->googleServiceDrive = new \Google_Service_Drive($client);
-        return $this->googleServiceDrive;
+            return $this->googleServiceDrive;
     }
-
+    
     /**
      * Set the set the Google Drivce Service instance.
      *
@@ -157,7 +157,7 @@ class GoogleDriveServiceProvider extends ServiceProvider
         $this->googleServiceDrive = $service;
         return $this;
     }
-
+    
     /**
      * Return the configuration for the Adpater
      *
@@ -171,10 +171,10 @@ class GoogleDriveServiceProvider extends ServiceProvider
             $this->config = new Config($config);
             $this->config->setFallback(new Config($fallback));
         }
-
+        
         return $this->config;
     }
-
+    
     /**
      * Register the application services.
      *
